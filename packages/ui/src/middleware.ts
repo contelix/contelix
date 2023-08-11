2106d3c2dd2b9019f7e5c81c11df99eab3ec486a
Martin Karry <martin@karry.de>
2023-08-11 10:56:15 +0200

    Removed unneeded console output

--- packages/ui/src/middleware.ts
+++ packages/ui/src/middleware.ts
@@ -3,7 +3,6 @@ import { NextRequest } from 'next/server'
 import { BASE_PATH_SERVICES } from './env';
 
 export function middleware(request: NextRequest) {
-    console.log(request.nextUrl.pathname)
     if (request.nextUrl.pathname.startsWith('/img')) {
         const headers = new Headers(request.headers);
         headers.set("fluffy-user", "martin");
